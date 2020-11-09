1ca3c2682cae560b2fe82b99e2ce0c0777b7b830
Bhargav N <20527213+bhargav516@users.noreply.github.com>
2020-11-09 14:49:43 +0530

    first commit

---
+++ src/com/creational/singleton/Runtime.java
@@ -0,0 +1,22 @@
+package com.creational.singleton;
+
+public class Runtime {
+
+    public static void main(String[] args) {
+	// Example for everyday singleton object
+
+        //trying to fetch runtime object
+        java.lang.Runtime singletonObject =  java.lang.Runtime.getRuntime();
+
+        System.out.println(singletonObject);
+
+        //another object
+        java.lang.Runtime anotherObject =  java.lang.Runtime.getRuntime();
+
+        System.out.println(anotherObject);
+
+        if (singletonObject == anotherObject){
+            System.out.println("Both objects are same");
+        }
+    }
+}
